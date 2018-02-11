312e29e59c52c46e969c6878cef1674854d9f139
Raquel Zempoalteca <rzempoalteca@csumb.edu>
2018-02-11 22:14:24 +0000

    Add all files

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+include './inc/functions.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> 777 Slot Machine </title>
+        <style>
+            @import url("css/styles.css");
+        </style>
+    </head>
+    <body>
+        
+    <div id = "main">
+        <?php
+            play();
+        ?>
+        <form>
+            <input type = "submit" value = "Spin!"/>
+        </form>
+    </div>
+        
+    </body>
+</html>
